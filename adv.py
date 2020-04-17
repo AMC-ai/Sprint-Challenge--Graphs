6292db2eaa60ffdfb84e0f1bd94eb557a55b3061
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-04-16 19:19:20 -0700

    trying to add suedo code that tracks a players movements and save the travled paths to print the 500 rooms

--- adv.py
+++ adv.py
@@ -5,6 +5,43 @@ from world import World
 import random
 from ast import literal_eval
 
+# utils
+
+
+# class Queue():
+#     def __init__(self):
+#         self.queue = []
+
+#     def enqueue(self, value):
+#         self.queue.append(value)
+
+#     def dequeue(self):
+#         if self.size() > 0:
+#             return self.queue.pop(0)
+#         else:
+#             return None
+
+#     def size(self):
+#         return len(self.queue)
+
+
+# class Stack():
+#     def __init__(self):
+#         self.stack = []
+
+#     def push(self, value):
+#         self.stack.append(value)
+
+#     def pop(self):
+#         if self.size() > 0:
+#             return self.stack.pop()
+#         else:
+#             return None
+
+#     def size(self):
+#         return len(self.stack)
+
+
 # Load world
 world = World()
 
@@ -17,7 +54,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -25,11 +62,49 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+# Start by writing an algorithm that picks a random unexplored direction from the player's current room, travels and logs that direction, then loops. This should cause your player to walk a depth-first traversal. When you reach a dead-end (i.e. a room with no unexplored paths), walk back to the nearest room that does contain an unexplored path.
+
+# You can find the path to the shortest unexplored room by using a breadth-first search for a room with a '?' for an exit. If you use the bfs code from the homework, you will need to make a few modifications.
+
+# Instead of searching for a target vertex, you are searching for an exit with a '?' as the value. If an exit has been explored, you can put it in your BFS queue like normal.
+
+# BFS will return the path as a list of room IDs. You will need to convert this to a list of n/s/e/w directions before you can add it to your traversal path.
+
+# If all paths have been explored, you're done!
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+# to implament a backwards path
+reverse_path = []
+# dictionaries for rooms and directions
+#rev_dic = {}
+#rm_dic = {}
+# keep track of rooms/paths visited
+#visited_path = {}
 
+# note:
+#  player.currentRoom.id starts at 0 and stores current room ID
+#  player.currentRoom.getExits() -  returns possible exits
+#  player.travel(direction) - Moves player through rooms
 
+# the first value with current Room of 0 will be set in the rooms dic and added to possible paths
+
+# create a while loop that runs through graphs paths that haven't been visited
+# if current room is not in visited
+# add current room to visited and rm dic
+# check reverse direction traveled
+# remove reverse direction traveled from room dic?
+
+# create a while loop to reverse out of room
+# remove/pop off rev path
+# add to que traversal path
+# travel reverse path
+
+# remove / pop exist room from list
+# add new traveled path to list
+# add new path to dic
+# move in that direction
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -41,11 +116,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
